d177fcb228c1655b7b25397d53cced0432c8111f
Oliver97 <oliverchong987@gmail.com>
2021-08-11 00:28:25 +0800

    Completed event emitters and how http works, stopped @ 4.03.25, Http Module Basics - 02-express-tutorial

--- 01-node-tutorial/13-event-emitter.js
+++ 01-node-tutorial/13-event-emitter.js
@@ -1,21 +1,25 @@
 // get back the class
 // if want custom extend from class
 // otherwise just for emitting and handling events create instance
-const EventEmitter = require('events')
+const EventEmitter = require("events");
 
-const customEmitter = new EventEmitter()
+const customEmitter = new EventEmitter();
 
 // on and emit methods
-// keep track of the order
+// keep track of the order. : MUST ALWAYS DECLARE BEFORE FUNCTION CALL
 // additional arguments
 // built-in modules utilize it
 
-customEmitter.on('response', (name, id) => {
-  console.log(`data recieved user ${name} with id:${id}`)
-})
+// listens for an event
+//this is something like a FUNCTION DECLARATION, to do whatever is in the function body on 'response' event
+customEmitter.on("response", (name, id) => {
+	console.log(`data recieved user ${name} with id:${id}`);
+});
 
-customEmitter.on('response', () => {
-  console.log('some other logic here')
-})
+//emits an event, this is something like a FUNCTION CALL
+customEmitter.on("response", () => {
+	console.log("some other logic here");
+});
 
-customEmitter.emit('response', 'john', 34)
+//FUNCTION CALL
+customEmitter.emit("response", "john", 34);
